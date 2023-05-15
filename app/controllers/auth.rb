d7c29377a3b8594977c892ffcba593c44e637654
a0716116z <65119444+a0716116z@users.noreply.github.com>
2023-05-16 00:53:40 +0800

    store&use auth token

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -17,18 +17,20 @@ module DramaConnect
         # POST /auth/login
         routing.post do
           
-          account = AuthenticateAccount.new(App.config).call(
+          account_info = AuthenticateAccount.new(App.config).call(
             username: routing.params['username'],
             password: routing.params['password']
           )
+          current_account = Account.new(account_info[:account_info],account_info[:auth_token])
 
-          SecureSession.new(session).set(:current_account, account)
-          flash[:notice] = "Welcome back #{account['username']}!"
-          routing.redirect "/account/#{account['username']}"
+          # SecureSession.new(session).set(:current_account, account)
+          CurrentSession.new(session).current_account = current_account
+          flash[:notice] = "Welcome back #{current_account.username}!"
+          routing.redirect "/account/#{current_account.username}"
         rescue AuthenticateAccount::UnauthorizedError
           print App.config.API_HOST
           flash.now[:error] = 'Username and password did not match our records'
-          response.status = 400
+          response.status = 401
           view :login
         rescue AuthenticateAccount::ApiServerError => e
           App.logger.warn "API server error: #{e.inspect}\n#{e.backtrace}"
@@ -40,8 +42,9 @@ module DramaConnect
 
       routing.on 'logout' do
         routing.get do
-          SecureSession.new(session).delete(:current_account)
+          # SecureSession.new(session).delete(:current_account)
           flash[:notice] = "You've been logged out"
+          CurrentSession.new(session).delete
           routing.redirect @login_route
         end
       end
---
+++ app/models/current_session.rb
@@ -0,0 +1,21 @@
+require_relative './current_account'
+
+module DramaConnect
+  class CurrentSession
+     def initialize(session)
+        @secure_session = session
+      end
+      def current_account
+        Account.new(@secure_session.get[:current_account],@secure_session.get[:auth_token]);
+      end
+      def current_account=
+        @secure_session.set(:current_account,account_info)
+        @secure_session.set(:auth_token,auth_token)
+      end
+      def delete
+        @secure_session.delete(:account)
+        @secure_session.delete(:auth_token)
+      end
+      end
+ 
+end
