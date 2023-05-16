7937367e1fdba9b33ad4d1b4371669eacd167e4b
Hsuan <lala092325@gmail.com>
2023-05-16 15:44:26 +0800

    modify register page and fix some errors

--- app/models/current_session.rb
+++ app/models/current_session.rb
@@ -1,21 +1,22 @@
-require_relative './current_account'
+require_relative './account'
 
 module DramaConnect
   class CurrentSession
      def initialize(session)
-        @secure_session = session
+        @secure_session = SecureSession.new(session)
       end
       def current_account
-        Account.new(@secure_session.get[:current_account],@secure_session.get[:auth_token]);
+        Account.new(@secure_session.get(:account),
+                    @secure_session.get(:auth_token))
       end
-      def current_account=
-        @secure_session.set(:current_account,account_info)
-        @secure_session.set(:auth_token,auth_token)
+      def current_account=(current_account)
+        @secure_session.set(:account, current_account.account_info)
+        @secure_session.set(:auth_token, current_account.auth_token)
       end
       def delete
         @secure_session.delete(:account)
         @secure_session.delete(:auth_token)
       end
       end
- 
+
 end
