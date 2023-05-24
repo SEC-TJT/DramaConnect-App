fea8e83637f6f567819bc14052ca4416afe90402
a0716116z <65119444+a0716116z@users.noreply.github.com>
2023-05-24 12:20:59 +0800

    login able

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,11 +8,11 @@ module DramaConnect
     attr_reader :account_info, :auth_token
 
     def username
-      @account_info ? @account_info['attributes']['username'] : nil
+      @account_info ? @account_info['username'] : nil
     end
 
     def email
-      @account_info ? @account_info['attributes']['email'] : nil
+      @account_info ? @account_info['email'] : nil
     end
 
     def logged_out?
