fa4b1f9e8b789ec3d58e0054de43a4f35c9d862f
a0716116z <65119444+a0716116z@users.noreply.github.com>
2023-05-24 12:42:33 +0800

    fix empty list

--- app/controllers/account.rb
+++ app/controllers/account.rb
@@ -14,6 +14,9 @@ module DramaConnect
 
           if @current_account && @current_account.username == username
             drama_lists = GetAllDramalists.new(App.config).call(@current_account)
+            # if drama_lists.failure?
+            #   puts 'yes'
+            # end
             print drama_lists
             view :account, locals: { current_account: @current_account ,drama_lists:  }
           else
--- app/services/get_all_dramalists.rb
+++ app/services/get_all_dramalists.rb
@@ -9,7 +9,6 @@ module DramaConnect
     def call(current_account)
       response = HTTP.auth("Bearer #{current_account.auth_token}")
                      .get("#{@config.API_URL}/dramaList")
-
       response.code == 200?JSON.parse(response.to_s)['data']:nil
 
     end
