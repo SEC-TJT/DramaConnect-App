caa55416b59a71f4dcc7698755ff47842fa019be
jerryho <jerryjp3914@gmail.com>
2023-05-26 02:21:29 +0800

    feature: add visitor by email

---
+++ app/services/remove_visitor.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module DramaConnect
+  # Service to add visitor to project
+  class RemoveVisitor
+    class VisitorNotRemoved < StandardError; end
+
+    def initialize(config)
+      @config = config
+    end
+
+    def api_url
+      @config.API_URL
+    end
+
+    def call(current_account:, visitor:, dramalist_id:)
+      response = HTTP.auth("Bearer #{current_account.auth_token}")
+                    .delete("#{api_url}/dramaList/#{dramalist_id}/visitors",
+                            json: { email: visitor[:email] })
+
+      raise VisitorNotRemoved unless response.code == 200
+    end
+  end
+end
