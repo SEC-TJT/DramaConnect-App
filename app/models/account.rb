4d1c7cf480d60d15ddbae916ce702f689fa803d6
Hsuan <lala092325@gmail.com>
2023-05-29 11:34:19 +0800

    fix showing github username and email

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,11 +10,11 @@ module DramaConnect
     attr_reader :account_info, :auth_token
 
     def username
-      @account_info ? @account_info['username'] : nil
+      @account_info ? @account_info['attributes']['username'] : nil
     end
 
     def email
-      @account_info ? @account_info['email'] : nil
+      @account_info ? @account_info['attributes']['email'] : nil
     end
 
     def logged_out?
